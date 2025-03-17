103920e9d6dc4a86945d03729513149eb08b14a6
Ahmedou <23083@gmail.com>
2025-03-17 02:08:30 -0700

    just me

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("since it work , i'm not sleeping US 18:29");
+        System.out.println("since it works, i'm not sleeping US 18:29");
     }
 }
